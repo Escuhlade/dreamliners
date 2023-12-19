620251a5a4119b6116f099104d06f163ffd119dd
Aaiyan <aaiyanvirji@gmail.com>
2023-12-19 01:20:33 -0500

    slider autoplay on showcase test

--- src/pages/Showcase.jsx
+++ src/pages/Showcase.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Slideshow from "../components/Slideshow";
 
 const Showcase = () => {
-  return <div>Showcase</div>;
+  return (
+    <div>
+      <Slideshow />
+    </div>
+  );
 };
 
 export default Showcase;
