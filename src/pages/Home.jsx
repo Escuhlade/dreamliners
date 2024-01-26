6006dfbc946a510b78ea37281f62156ad5ee34d1
Escuhlade <aaiyanvirji@gmail.com>
2024-01-25 21:23:03 -0500

    images upload for exhibit & showcase changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,7 +46,8 @@ const Home = () => {
             autoPlay
             loop
           >
-            <source src="https://rr5---sn-tt1e7nlz.c.drive.google.com/videoplayback?expire=1704181883&ei=SpaTZdeAPZXdwN4PvO2a-A8&ip=99.237.254.43&id=495544872713bdbd&itag=22&source=webdrive&requiressl=yes&xpc=EghonaK1InoBAQ==&mh=mL&mm=32&mn=sn-tt1e7nlz&ms=su&mv=m&mvi=5&pl=18&ttl=transient&susc=dr&driveid=1-Gjr0qahvO4OhBZNXG49UqlsNZIblwzV&app=explorer&eaua=WIug9EHVF8Q&mime=video/mp4&vprv=1&prv=1&dur=10.031&lmt=1703110121549697&mt=1704170697&subapp=DRIVE_WEB_FILE_VIEWER&txp=0006224&sparams=expire,ei,ip,id,itag,source,requiressl,xpc,ttl,susc,driveid,app,eaua,mime,vprv,prv,dur,lmt&sig=AJfQdSswRQIhAM47PjDWZ5n7eUFc1tDSNEVNRfHemdR88899XRKgTkKwAiAh885AkIK3owZ24XUfBSR_pKX1uSMQq_HbO1kjGVow6g==&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AAO5W4owRgIhAKN426Sxm-vnCaEWu6633FO-lyReYzQKq4NaF9j661BtAiEAzV1GlPHhI9ZrWInDPh-tcvv77T0ezkImHKaJ5NI1bOE=&cpn=0B1wGHqvgbFmzrV5&c=WEB_EMBEDDED_PLAYER&cver=1.20231217.00.00" />
+            {/* Below is the main hero video that is going to be on the landing page of the website in source */}
+            <source src="https://rr5---sn-tt1e7nlz.c.drive.google.com/videoplayback?expire=1706235214&ei=HuuyZY2zB7C2mvUP0fanCA&ip=209.171.88.88&id=495544872713bdbd&itag=37&source=webdrive&requiressl=yes&xpc=EghonaK1InoBAQ==&mh=mL&mm=32,29&mn=sn-tt1e7nlz,sn-uxa0n-tm3e&ms=su,rdu&mv=m&mvi=5&pl=24&ttl=transient&susc=dr&driveid=1-Gjr0qahvO4OhBZNXG49UqlsNZIblwzV&app=explorer&eaua=_gCHqaHYbww&mime=video/mp4&vprv=1&prv=1&dur=10.031&lmt=1703111205606126&mt=1706224132&fvip=3&subapp=DRIVE_WEB_FILE_VIEWER&txp=0001224&sparams=expire,ei,ip,id,itag,source,requiressl,xpc,ttl,susc,driveid,app,eaua,mime,vprv,prv,dur,lmt&sig=AJfQdSswRgIhAKuLmCjK9yyVHpCZyr0-YxNT1eFdL481RZxO58_l2XdDAiEAgo40Wy8pvsYd_xN2IzPeL39bDgNwDsd499mv8_FtGTw=&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AAO5W4owRQIhAMP2k6SaYqH3XjZcOSaTCLHRHupUHiO1wDJCJvOt26L4AiB-5I-S6Ny_doCMJMQ2NByxnGO69wpF_M5Ss-ANkNwWIg==&cpn=6sYCzg3VoljYxBcR&c=WEB_EMBEDDED_PLAYER&cver=1.20240123.01.00" />
             Your browser does not support the video tag.
           </video>
           {/* <VideoHeroPlay /> */}
@@ -61,7 +62,7 @@ const Home = () => {
           </video> */}
         </div>
         <div className="flex justify-start snap-always snap-center mb-0 h-screen">
-          {/* left video */}
+          {/* left video for half half split*/}
           <video
             className="w-auto h-full object-cover"
             controls={false}
@@ -69,13 +70,13 @@ const Home = () => {
             loop
           >
             <source
-              src="https://rr1---sn-tt1e7nls.c.drive.google.com/videoplayback?expire=1704182773&ei=xZmTZcqlC5TcwN4P48iLoA8&ip=99.237.254.43&id=ddd9a9ae7659a8a2&itag=22&source=webdrive&requiressl=yes&xpc=EghonaK1InoBAQ==&mh=JS&mm=32&mn=sn-tt1e7nls&ms=su&mv=m&mvi=1&pl=18&ttl=transient&susc=dr&driveid=1vG3U3dwPAxzM5hFafEtOHormIfZkSQys&app=explorer&eaua=WIug9EHVF8Q&mime=video/mp4&vprv=1&prv=1&dur=12.329&lmt=1703110133397794&mt=1704171649&subapp=DRIVE_WEB_FILE_VIEWER&txp=0006224&sparams=expire,ei,ip,id,itag,source,requiressl,xpc,ttl,susc,driveid,app,eaua,mime,vprv,prv,dur,lmt&sig=AJfQdSswRQIgNvRbpbcp-yVBofPdStgn-M0bINytwIVfho71YHTkD_ICIQCPri4tjqLpcNSftlFh166-Tja5k9XbIIvpN8E6ndtVYA==&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AAO5W4owRgIhAK_wve3lN-x2MILuj-We1EpRSgCWIGlM16vrPpSLliv3AiEArHfwALybW3AiucQaoS6qRkjDSRjOgcIAiAhMl1aAePM=&cpn=5TE99G5-ECtq8VIa&c=WEB_EMBEDDED_PLAYER&cver=1.20231217.00.00"
+              src="https://rr1---sn-tt1e7nls.c.drive.google.com/videoplayback?expire=1706235636&ei=xOyyZYzjGri3mvUP5KCikA0&ip=207.112.12.19&id=ddd9a9ae7659a8a2&itag=37&source=webdrive&requiressl=yes&xpc=EghonaK1InoBAQ==&mh=JS&mm=32,29&mn=sn-tt1e7nls,sn-gvbxgn-tt1e6&ms=su,rdu&mv=m&mvi=1&pl=22&ttl=transient&susc=dr&driveid=1vG3U3dwPAxzM5hFafEtOHormIfZkSQys&app=explorer&eaua=_gCHqaHYbww&mime=video/mp4&vprv=1&prv=1&dur=12.329&lmt=1703110802182701&mt=1706224381&fvip=6&subapp=DRIVE_WEB_FILE_VIEWER&txp=0001224&sparams=expire,ei,ip,id,itag,source,requiressl,xpc,ttl,susc,driveid,app,eaua,mime,vprv,prv,dur,lmt&sig=AJfQdSswRQIgVsRwukRZwWS90lGFXb-4wYyn-hNWWHpexsiWLjLIbjQCIQDz7lz3BbaWs20LofdfZHR_0ATzCxPOVNfGBvlsWM3PVw==&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AAO5W4owRQIgSd2Sn2BLtsrHe_r-6jB5kiO62j7fp37p896Zo7JuobkCIQCNqABJIYxwQa4pNTeAz2V8zcAEN_vZJOlkukHaFguh6g==&cpn=VFcE7UHq14sWhP69&c=WEB_EMBEDDED_PLAYER&cver=1.20240123.01.00"
               type="video/mp4"
             />
             Your browser does not support the video tag.
           </video>
 
-          {/* right video */}
+          {/* right video for half half split*/}
           <video
             className="w-full h-full object-cover"
             controls={false}
@@ -83,7 +84,7 @@ const Home = () => {
             loop
           >
             <source
-              src="https://rr3---sn-tt1elnel.c.drive.google.com/videoplayback?expire=1704182659&ei=U5mTZZf4Ff-r-LYPh8Wb2AQ&ip=99.237.254.43&id=b081ba1945a72c26&itag=22&source=webdrive&requiressl=yes&xpc=EghonaK1InoBAQ==&mh=IM&mm=32&mn=sn-tt1elnel&ms=su&mv=m&mvi=3&pl=18&ttl=transient&susc=dr&driveid=1maH2XKnlJvYMZJmgL2PMEI8FdtO1ypTL&app=explorer&eaua=WIug9EHVF8Q&mime=video/mp4&vprv=1&prv=1&dur=11.888&lmt=1703110102658367&mt=1704171649&subapp=DRIVE_WEB_FILE_VIEWER&txp=0006224&sparams=expire,ei,ip,id,itag,source,requiressl,xpc,ttl,susc,driveid,app,eaua,mime,vprv,prv,dur,lmt&sig=AJfQdSswRAIgII7B3PUc_Vum55w8fEHagTrmM0CRlMa5RSTn_kdumzsCICg7DzK8urFiNEOynsYvI3RvPpv9ArYBlFyu1H-X9wy7&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AAO5W4owRgIhANS0OvRwbQJ4jCtdQs2HB3G0-icsrpwy_vzyDLeL1L_zAiEAhKlVZQCnq6DhnfGol2wKblYQ1t-12jNxMBwzU2L3Ou4=&cpn=-sENWyKeyfRNE1j-&c=WEB_EMBEDDED_PLAYER&cver=1.20231217.00.00"
+              src="https://rr5---sn-tt1e7nls.c.drive.google.com/videoplayback?expire=1706235385&ei=yeuyZd-UC-rBp84P4JOz8Ac&ip=209.171.88.88&id=b081ba1945a72c26&itag=37&source=webdrive&requiressl=yes&xpc=EghonaK1InoBAQ==&mh=IM&mm=32,29&mn=sn-tt1e7nls,sn-uxa0n-tm3l&ms=su,rdu&mv=m&mvi=5&pl=24&ttl=transient&susc=dr&driveid=1maH2XKnlJvYMZJmgL2PMEI8FdtO1ypTL&app=explorer&eaua=_gCHqaHYbww&mime=video/mp4&vprv=1&prv=1&dur=11.888&lmt=1703110207433392&mt=1706224132&fvip=1&subapp=DRIVE_WEB_FILE_VIEWER&txp=0001224&sparams=expire,ei,ip,id,itag,source,requiressl,xpc,ttl,susc,driveid,app,eaua,mime,vprv,prv,dur,lmt&sig=AJfQdSswRAIgMXtRx78iZshNFF7_ka67a1vaiHVyXD9TeRqXvxaPZP8CIAQFCoeTsOeiEVHB_dOrxWecA6lU_uwaEOpl9Qr_eDiw&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AAO5W4owRQIgQ7jtnMKy0A-2qVGJ3ei7CxmBX7ImdTUnNYlYGxwKxHcCIQCket-sFayf6PIa5d7-UAI5-_ZccN9LxSb3zEzH_hBBmQ==&cpn=jZZPn0VSugr528Ry&c=WEB_EMBEDDED_PLAYER&cver=1.20240123.01.00"
               type="video/mp4"
             />
             Your browser does not support the video tag.
@@ -96,6 +97,7 @@ const Home = () => {
             autoPlay
             loop
           >
+            {/* footer video end of website */}
             <source
               src="https://rr1---sn-tt1e7nls.c.drive.google.com/videoplayback?expire=1704182794&ei=2pmTZeaTCvT1mvUPvtSUEA&ip=99.237.254.43&id=4cff8e21831e3c50&itag=22&source=webdrive&requiressl=yes&xpc=EghonaK1InoBAQ==&mh=Ls&mm=32&mn=sn-tt1e7nls&ms=su&mv=m&mvi=1&pl=18&ttl=transient&susc=dr&driveid=1sVWG-P9GC3F7OU173ZzrOaZOZl9HHmOE&app=explorer&eaua=WIug9EHVF8Q&mime=video/mp4&vprv=1&prv=1&dur=10.054&lmt=1704093322518614&mt=1704171640&subapp=DRIVE_WEB_FILE_VIEWER&txp=0006224&sparams=expire,ei,ip,id,itag,source,requiressl,xpc,ttl,susc,driveid,app,eaua,mime,vprv,prv,dur,lmt&sig=AJfQdSswRQIhAPbZgu-HzbZJomRs5NKQAkI4psbhP4fRY4zZrpiLSR5WAiBJcO-9gGt39n7KcnjSV1Dp4ON_cfbjA4xULH7CdfiUJg==&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AAO5W4owRAIgboDFafI5JUoIx_wWsfCcR8IY9wRbLtgc1i-LrBS-HAQCIHNpcPWX0NDhM0Fefw3NBTkId285LgotNyn8bunucZzF&cpn=wRLTv6Ui73ztuiix&c=WEB_EMBEDDED_PLAYER&cver=1.20231217.00.00"
               type="video/mp4"
--- src/pages/Showcase.jsx
+++ src/pages/Showcase.jsx
@@ -7,8 +7,15 @@ import ImageSlider from "../components/ImageSlider";
 const Showcase = () => {
   return (
     <div>
+      {/* general wrap for navbar */}
       <Navbar />
+      <div>
+      {/* this is for the image sliding and the packages in the on exihbit on figma */}
+      {/* image slider component */}
       <ImageSlider />
+      {/* ultra luxury package */}
+      <img src="../images/Exhibit_Images/m801.png" alt="M8 Starlight Kit"className="w-full h-full"></img>
+      </div>
       {/* <Slideshow /> */}
     </div>
   );
