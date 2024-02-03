18d512cb8c46f8efc4d9f99891b5da2695df2603
Escuhlade <aaiyanvirji@gmail.com>
2024-02-03 04:04:34 -0500

    snap scroll working on home page functional

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useRef } from "react";
-import useScrollSnap from "react-use-scroll-snap";
+import React, { useRef, useEffect } from "react";
 import Navbar from "../components/Navbar";
-import HeroVideo from "../videos/HeroVideo.mp4";
-import VideoHeroPlay from "../components/VideoHeroPlay";
 import FooterComp from "../components/FooterComp";
 
 import herovideo from '../videos/compressed/HeroVideo-trimmed.mp4'
@@ -11,18 +7,18 @@ import dualcolor from '../videos/compressed/DualColor-trimmed.mp4'
 import leftside from '../videos/compressed/LeftSide-trimmed.mp4'
 import homepage3 from '../videos/compressed/HomePage3-Trimmed.mov'
 
-
 const Home = () => {
-  const scrollRef = useRef(null);
-  useScrollSnap({ ref: scrollRef, duration: 100, delay: 20 });
-  
+  const videoRefs = [useRef(), useRef(), useRef()];
+
+  useEffect(() => {
+  }, []);
+
   return (
     <>
-      {/* <h1> Butcher Works</h1> */}
       <Navbar />
-      <div className="overflow-y-auto h-screen" ref={scrollRef}>
-        <div className="mb-0 w-full h-full">
-          {/* <ReactVideoPlayer /> */}
+      <div className="overflow-y-auto h-screen" style={{ scrollSnapType: "y mandatory" }}>
+        {/* First video div */}
+        <div ref={videoRefs[0]} className="mb-0 w-full h-full" style={{ scrollSnapAlign: "start" }}>
           <video
             className="w-full h-full object-cover"
             controls={false}
@@ -30,23 +26,13 @@ const Home = () => {
             loop={true}
             muted={true}
           >
-            {/* Below is the main hero video that is going to be on the landing page of the website in source */}
             <source src={herovideo} />
             Your browser does not support the video tag.
           </video>
-          {/* <VideoHeroPlay /> */}
-          {/* <waqVideo /> */}
-          {/* <video
-            className="w-full h-full"
-            controls
-            onError={(e) => console.error(e)}
-          >
-            <source src={HeroVideo} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video> */}
         </div>
-        <div className="flex justify-start mb-0 h-screen">
-          {/* left video for half half split*/}
+
+        {/* Second video div */}
+        <div ref={videoRefs[1]} className="flex justify-start mb-0 h-screen" style={{ scrollSnapAlign: "start" }}>
           <video
             className="w-auto h-full object-cover"
             controls={false}
@@ -54,14 +40,9 @@ const Home = () => {
             loop={true}
             muted={true}
           >
-            <source
-              src={leftside}
-              type="video/mp4"
-            />
+            <source src={leftside} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
-
-          {/* right video for half half split*/}
           <video
             className="w-full h-full object-cover"
             controls={false}
@@ -69,14 +50,13 @@ const Home = () => {
             loop={true}
             muted={true}
           >
-            <source
-              src={dualcolor}
-              type="video/mp4"
-            />
+            <source src={dualcolor} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
-        <div className="mb-0 h-screen">
+
+        {/* Third video div */}
+        <div ref={videoRefs[2]} className="mb-0 h-screen" style={{ scrollSnapAlign: "start" }}>
           <video
             className="w-full h-full object-cover"
             controls={false}
@@ -84,17 +64,14 @@ const Home = () => {
             loop={true}
             muted={true}
           >
-            {/* footer video end of website */}
-            <source
-              src={homepage3}
-              type="video/mp4"
-            />
+            <source src={homepage3} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
       </div>
-      {/* footer comp */}
-      <FooterComp/>
+
+      {/* Footer component */}
+      <FooterComp />
     </>
   );
 };
