18df332fb334f1b752f09766bfba76e23ab41dd3
Aaiyan Virji <aaiyanvirji@gmail.com>
2024-03-03 22:22:23 -0500

    uzi dog footer changes who can even read this

--- src/components/FooterComp.jsx
+++ src/components/FooterComp.jsx
@@ -1,27 +1,44 @@
 // FooterComp.js
-import { Footer } from 'flowbite-react';
+import { Footer } from "flowbite-react";
 
 function FooterComp() {
-
   return (
-    <div className=''>
-      <nav className="bg-transparent w-full z-10">
-        <div className="flex mx-16 items-center p-4 pt-14 justify-center">
-          <div className='flex flex-col space-y-1 text-white text-sm text-left h-full w-1/5'>
-            <a href='/'><p>Home</p></a>
-            <a href='/about'><p>About Us</p></a>
-            <a href='/showcase'><p>Gallery</p></a>
-            <a href='/packages'><p>Packages</p></a>
-            <a href='/bookings'><p>Bookings</p></a>
-            <a href='/faqs'><p>FAQs</p></a>
-            <a href='/contact'><p>Contact</p></a>
+    <div className="">
+      <nav className="z-10 w-full bg-transparent">
+        <div className="mx-16 flex items-center justify-center p-4 pt-14">
+          <div className="space-y1-1 flex flex-row text-left text-lg text-white">
+            <div className="flex h-full w-1/5 flex-col space-y-1 pl-2 text-left text-lg text-white">
+              <a href="/">
+                <p>Home</p>
+              </a>
+              <a href="/about">
+                <p>AboutUs</p>
+              </a>
+              <a href="/showcase">
+                <p>Gallery</p>
+              </a>
+            </div>
+            <div className="flex h-full w-1/5 flex-col space-y-1 pl-20 text-left text-lg text-white">
+              <a href="/packages">
+                <p>Packages</p>
+              </a>
+              <a href="/bookings">
+                <p>Bookings</p>
+              </a>
+              <a href="/faqs">
+                <p>FAQs</p>
+              </a>
+              <a href="/contact">
+                <p>Contact</p>
+              </a>
+            </div>
           </div>
-          <div className="flex flex-col items-center justify-center h-full w-full">
+          <div className="flex h-full w-full flex-col items-center justify-center">
             <div className="flex items-center justify-center">
               <a href="/">
                 <img
                   src="https://i.gyazo.com/8727f9cdf2e8b2ad922a4738cf995a0f.png"
-                  className="h-16 cursor-pointer transform transition-transform duration-100 hover:scale-110"
+                  className="h-16 transform cursor-pointer transition-transform duration-100 hover:scale-110"
                   alt="DL Logo"
                 />
               </a>
@@ -29,28 +46,32 @@ function FooterComp() {
             <a href="https://www.instagram.com/dreamliners.cp/">
               <img
                 src="https://images2.imgbox.com/3b/19/w1oJKniW_o.png"
-                className="h-8 cursor-pointer transform transition-transform duration-100 hover:scale-110"
+                className="h-8 transform cursor-pointer transition-transform duration-100 hover:scale-110"
                 alt="DL Logo"
               />
             </a>
           </div>
-          <div className="flex flex-col justify-end h-full w-1/5">
-            <a href='https://www.instagram.com/dreamliners.cp/'>
-              <div className="flex items-center justify-end" >
-                <p className='text-white text-sm text-right w-full py-2'>@dreamliners.cp</p>
+          <div className="flex h-full w-1/5 flex-col justify-end">
+            <a href="https://www.instagram.com/dreamliners.cp/">
+              <div className="flex items-center justify-end">
+                <p className="w-full py-2 text-right text-lg text-white">
+                  @dreamliners.cp
+                </p>
                 <img
                   src="https://i.gyazo.com/6aa589114f810d037cd078b07c45ee97.png"
-                  className="w-10 h-10"
+                  className="h-10 w-10"
                   alt="Some Other Logo"
                 />
               </div>
             </a>
-            <a href='https://www.facebook.com/profile.php?id=61552362267687'>
+            <a href="https://www.facebook.com/profile.php?id=61552362267687">
               <div className="flex items-center justify-end">
-                <p className='text-white text-sm text-right w-full py-2'>Dreamliners CP</p>
+                <p className="w-full py-2 text-right text-lg text-white">
+                  Dreamliners CP
+                </p>
                 <img
                   src="https://i.gyazo.com/6aa589114f810d037cd078b07c45ee97.png"
-                  className="w-10 h-10"
+                  className="h-10 w-10"
                   alt="Some Other Logo"
                 />
               </div>
@@ -58,11 +79,13 @@ function FooterComp() {
           </div>
         </div>
         <div style={{ scrollSnapAlign: "end" }}>
-          <p className='text-white text-sm text-center w-full py-5'>© 2024 Dreamliners</p>
+          <p className="w-full py-5 text-center text-sm text-white">
+            © 2024 Dreamliners
+          </p>
         </div>
       </nav>
     </div>
-  )
+  );
 }
 
 export default FooterComp;
